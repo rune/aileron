7932707af8aba3329be3c3e1f8d9ece28e5f157e
Sanjay Guruprasad <sanjay.guruprasad@gmail.com>
2019-04-11 17:01:21 -0700

    Rename and improve validateTypes
    formerly validateInputTypes

---
+++ src/lib/validateTypes.js
@@ -0,0 +1,26 @@
+const { typeCheck } = require("type-check")
+
+const validateTypes = (typeDefinitions, receivedInputs = {}) => {
+  try {
+    let parsedInputs = {}
+    for (const inputName in typeDefinitions) {
+      if (typeCheck(typeDefinitions[inputName], receivedInputs[inputName])) {
+        parsedInputs[inputName] = receivedInputs[inputName]
+      } else {
+        return {
+          inputErr: {
+            err: { [inputName]: receivedInputs[inputName] },
+            msg: `Aileron type error: "${inputName}" of type ${
+              typeDefinitions[inputName]
+            } should be provided. Received "${receivedInputs[inputName]}" instead.`
+          }
+        }
+      }
+    }
+    return { parsedInputs }
+  } catch (err) {
+    throw { message: "Aileron type validation error", err: err }
+  }
+}
+
+module.exports = validateTypes
